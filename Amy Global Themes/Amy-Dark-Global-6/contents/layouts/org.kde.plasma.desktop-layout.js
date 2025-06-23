df6c0ebcb637f4a31b7af04c551d975581800a86
James <jenningsje@mac.com>
2025-06-23 13:53:51 -0500

    test it

--- Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
+++ Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,7 +1,3 @@
-// layout.js — load the amy panel layout and set wallpaper plugin for desktops
-
-loadTemplate("amy-panel-layout");  // load your detailed panel layout
-
 // Set wallpaper plugin for all desktops on current activity
 var desktopsArray = desktopsForActivity(currentActivity());
 for (var j = 0; j < desktopsArray.length; j++) {
