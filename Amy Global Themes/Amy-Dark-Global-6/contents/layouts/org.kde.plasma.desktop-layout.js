12d719d7e5246921a533210b09cf4aeaebbd747c
James <jenningsje@mac.com>
2025-06-24 11:12:41 -0500

    work on not setting up the wallpaper

--- Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
+++ Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,1 +1,5 @@
-loadTemplate("amy-panel-layout");  // load your detailed panel layout
+var desktopsArray = desktopsForActivity(currentActivity());
+for (var j = 0; j < desktopsArray.length; j++) {
+    // Example: only set some other config, but don't touch wallpaper
+    // desktopsArray[j].someOtherProperty = 'something';
+}
