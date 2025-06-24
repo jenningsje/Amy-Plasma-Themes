37b9958b5d9377ca860b0439ac53eeeea3685c66
James <jenningsje@mac.com>
2025-06-24 11:18:07 -0500

    empty org.kde.plasma.desktop-layout.js

--- Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
+++
@@ -1,5 +0,0 @@
-var desktopsArray = desktopsForActivity(currentActivity());
-for (var j = 0; j < desktopsArray.length; j++) {
-    // Example: only set some other config, but don't touch wallpaper
-    // desktopsArray[j].someOtherProperty = 'something';
-}
