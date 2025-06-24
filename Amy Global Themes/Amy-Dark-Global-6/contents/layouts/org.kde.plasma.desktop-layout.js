a904b908ffabbf56a48e8f38e92317ddeb6746f1
James <jenningsje@mac.com>
2025-06-24 09:13:48 -0500

    just has panel now

--- Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
+++ Amy Global Themes/Amy-Dark-Global-6/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,8 +1,1 @@
 loadTemplate("amy-panel-layout");  // load your detailed panel layout
-
-// Set wallpaper plugin for all desktops on current activity
-var desktopsArray = desktopsForActivity(currentActivity());
-for (var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
-}
-
